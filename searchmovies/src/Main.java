5ded2ee39e2d0e5acf5f4012d1ec0a1b71657781
D5error <D5error@163.com>
2024-02-14 20:36:15 +0800

    2024/2/14

--- searchmovies/src/Main.java
+++ searchmovies/src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Movie[] movies = new Movie[4];
+        movies[0] = new Movie(1, "name1", 1, 1, "director1", "actor1", "info1");
+        movies[1] = new Movie(2, "name2", 2, 2, "director2", "actor2", "info2");
+        movies[2] = new Movie(3, "name3", 3, 3, "director3", "actor3", "info3");
+        movies[3] = new Movie(4, "name4", 4, 4, "director4:", "actor4", "info4");
+
+        MovieOperator operator = new MovieOperator(movies);
+        operator.printAllMovies();
+        operator.serachMovieById(1);
+        operator.serachMovieById(2);
+        operator.serachMovieById(3);
+        operator.serachMovieById(4);
+        operator.serachMovieById(5);
     }
 }
--- searchmovies/src/MovieOperator.java
+++ searchmovies/src/MovieOperator.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class MovieOperator {
+public class MovieOperator {
+    private Movie[] movies;
+    public MovieOperator(Movie[] movies){
+        this.movies = movies;
+    }
+
+    /** 展示系统全部电影信息 */
+    public void printAllMovies(){
+        System.out.println("系统全部电影信息如下：");
+        for (int i = 0; i < movies.length; i++) {
+            Movie m = movies[i];
+            System.out.println("编号：" + m.getId());
+            System.out.println("名称：" + m.getName());
+            System.out.println("价格：" + m.getPrice());
+            System.out.println();
+        }
+    }
+
+    /** 根据电影编号查询该电影的详细信息 */
+    public void serachMovieById(int id){
+        for (int i = 0; i < movies.length; i++) {
+            Movie m = movies[i];
+            if(m.getId() == id){
+                System.out.println("该电影详情如下：");
+                System.out.println("编号：" + m.getId());
+                System.out.println("名称：" + m.getName());
+                System.out.println("价格：" + m.getPrice());
+                System.out.println("得分：" + m.getScore());
+                System.out.println("导演：" + m.getDirector());
+                System.out.println("主演：" + m.getActor());
+                System.out.println("其它信息：" + m.getInfo());
+                return;
+            }
+        }
+        System.out.println("没有该电影信息");
+    }
 }
